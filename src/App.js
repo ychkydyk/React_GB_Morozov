b26f43d3472c29c32c37f30ff046af4f3f70de22
ychkydyk <zavdv1@gmail.com>
2022-12-08 18:40:12 +0300

    +add bot

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 
 import {MessageList} from "./components/MessageList/MessageList";
 import {Form} from "./components/Form/Form";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 
 export function App() {
-    const [messages, setMessages] = useState([
-        {
-            author:'Biba',
-            text: '1'
-         },
-        {
-            author:'Boba',
-            text: '2'
-        },
-        {
-            author:'Giga',
-            text: '3'
-        },
-        {
-            author:'Goga',
-            text: '4'
-        },
-    ])
+    const [messages, setMessages] = useState([])
 
     const addMessage = (newMessage) => {
         setMessages([...messages, newMessage])
     }
+    useEffect(()=>{
+        if (messages.length > 0) {
+           const timeout = setTimeout(() => {
+                addMessage({
+                    author: 'bot',
+                    text: 'Im Bot'
+                })
+            }, 1500)
+            return() => {
+                clearTimeout(timeout)
+            }
+        }
+
+    },[messages])
 
 
     return (
